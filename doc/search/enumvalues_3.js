74c5b6fdfb543aac710993f1a7e6fb48ae341a60
ZSaberLv0 <z@zsaber.com>
2025-01-11 05:32:21 +0000

    update doc

---
+++ doc/search/enumvalues_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['value_0',['Value',['../classzft_is_same.html#aa859ac25c2ec0b716308645aab2ed9bea1deec6dec2f13da530cd88ef0ce2e98b',1,'zftIsSame::Value'],['../classzft_is_class.html#aba3f10ac4d53ec085621289b0ad2a709a0d1bb4908abc15cebeb69b815093a690',1,'zftIsClass::Value'],['../classzft_is_type_of.html#aacd9981a4783ef395d490e38733916efaa9a76f85343637bc8a98d2e3004441af',1,'zftIsTypeOf::Value'],['../classzft_is_p_o_d.html#a44a6d67d96a2779f78d8ba888f62e2b3af45c9a527c420382e5a5678f70bd785a',1,'zftIsPOD::Value']]]
+];
